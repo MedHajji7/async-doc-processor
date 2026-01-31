3a7d5eda020f1b2e69ae8eaaaf09e49e4c8ada7e
medhajji11 <medsii091@gmail.com>
2026-01-31 03:58:56 +0100

    feat : demonstrate job failures and retry handling

--- app/Events/DocumentUploaded.php
+++ app/Events/DocumentUploaded.php
@@ -11,10 +11,11 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Document;
+
 class DocumentUploaded
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-    
+
     public Document $document;
 
     /**
